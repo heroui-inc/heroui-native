1af6ae5215e24a2c95af670fcc2920dc2dd3b593
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2025-09-23 16:35:31 +0300

    chore: update number of showcases in example app

--- example/src/app/(home)/index.tsx
+++ example/src/app/(home)/index.tsx
@@ -53,7 +53,7 @@ const cards: HomeCardProps[] = [
       'https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/images/heroui-native-example/home-showcases-light.png',
     imageDark:
       'https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/images/heroui-native-example/home-showcases-dark-1.png',
-    count: 2,
+    count: 3,
     footer: 'View components in action',
     path: 'showcases',
   },

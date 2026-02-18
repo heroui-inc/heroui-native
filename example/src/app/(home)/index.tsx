faa252e120921f5feb1d0184046bc7b117221e74
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2026-02-18 19:57:07 +0200

    build: update deps as per expo doctor

--- example/src/app/(home)/index.tsx
+++ example/src/app/(home)/index.tsx
@@ -157,7 +157,7 @@ export default function App() {
   return (
     <ScreenScrollView>
       <View className="items-center justify-center my-4">
-        <AppText className="text-muted text-base">v1.0.0-rc.1</AppText>
+        <AppText className="text-muted text-base">v1.0.0-rc.2</AppText>
       </View>
       <View className="gap-6">
         {cards.map((card, index) => (

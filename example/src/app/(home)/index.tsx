21604216167abf5def8c5faa83edd691630fb812
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2026-03-06 13:47:59 +0200

    fix(example-app): index screen text issue

--- example/src/app/(home)/index.tsx
+++ example/src/app/(home)/index.tsx
@@ -160,9 +160,9 @@ export default function App() {
 
   return (
     <ScreenScrollView>
-      <View className="items-center justify-center my-4">
-        <AppText className="text-muted text-base">v1.0.0-rc.3</AppText>
-      </View>
+      <AppText className="text-muted text-base text-center my-4">
+        v1.0.0-rc.3
+      </AppText>
       <View className="gap-6">
         {cards.map((card, index) => (
           <HomeCard

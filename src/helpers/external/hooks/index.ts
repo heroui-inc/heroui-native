da8fbfa7c04993cc684df76ef5783cf61a23fcd5
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2026-03-29 15:19:32 +0300

    feat: export useGlobalAnimationSettings hook from public API

--- src/helpers/external/hooks/index.ts
+++ src/helpers/external/hooks/index.ts
@@ -1,3 +1,4 @@
+export { useGlobalAnimationSettings } from '../../../providers/animation-settings/index';
 export { useTextComponent } from '../../../providers/text-component/index';
 export * from './use-bottom-sheet-aware-handlers';
 export * from './use-is-on-surface';

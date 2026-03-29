9ca1d66eecf1e78d49bb86815531bb04d3c552c9
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2026-03-29 15:50:21 +0300

    feat: export use-combined-animation-disabled-state hook from public API

--- src/helpers/external/hooks/index.ts
+++ src/helpers/external/hooks/index.ts
@@ -1,5 +1,5 @@
-export { useGlobalAnimationSettings } from '../../../providers/animation-settings/index';
 export { useTextComponent } from '../../../providers/text-component/index';
+export * from '../../internal/hooks/use-combined-animation-disabled-state';
 export * from './use-bottom-sheet-aware-handlers';
 export * from './use-is-on-surface';
 export * from './use-theme-color';

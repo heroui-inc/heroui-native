aef5d44b08bce86b63d9920b0c381930b0c46735
Aron <2425532357@qq.com>
2026-08-26 15:31:00 +0800

    feat: remove default border radius on LinkButton

--- src/components/link-button/link-button.styles.ts
+++ src/components/link-button/link-button.styles.ts
@@ -2,7 +2,7 @@ import { tv } from '../../helpers/external/utils/cn';
 import { combineStyles } from '../../helpers/internal/utils';
 
 const root = tv({
-  base: 'link-button__root',
+  base: 'link-button__root rounded-none',
 });
 
 const linkButtonClassNames = combineStyles({
